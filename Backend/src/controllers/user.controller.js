b4ad5759d5fb532d9abe038d4b7b197f2094fb3b
ednarocio94 <ednarociorodriguez94@gmail.com>
2024-11-14 19:50:47 -0500

    Actualización de carpetas

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,80 @@
+// ACÁ VA LA LÓGICA DE LOS CONTROLADORES PARA LAS PETICIONES HTTP PARA LOS USUARIOS
+// Nos estaremos centrando en las peticiones POST Y GET
+
+// 1. Importar dependencias y módulos que necesitemos
+import { userModel } from "../models/users.model.js";
+// Importar la dependenciade encriptación
+import bcrypt from "bcryptjs";
+
+// 2. Crearnos nuestras funciones asíncronas para cada petición
+//function nombreFuncion(){}
+//let nombreFuncion2 = ()=>{}
+
+
+// --------------------------------------------------
+// Petición POST -> Crear usuarios
+export const createUser = async (req, res) => {
+  // manejo de errores -> atrapar lo que pueda salir mal
+  try {
+
+    // Deestructuración -> nos va a permitir acceder a cada una de las variables suministradas por el usuario en el req.body
+    const {fullName, email, password, role} = req.body;
+
+    // password = sancocho;
+
+    // vamos a encriptar la contraseña
+    // .hash -> método para encriptar contraseña
+    // 2 parámetros, 1. contraseña a encriptar
+    //               2. # que determina cuán segura es la contraseña encriptada -> 8-10
+    const codedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create({
+        fullName,
+        email,
+        password:codedPassword,
+        role
+    });
+
+    // 201-> se creó correctamente
+    return res.status(201).json({
+        mensaje: "Usuario creado correctamente",
+        datos: newUser
+    });
+
+  } catch (error) {
+    return res.status(400).json({
+        mensaje: "Ocurrió un error al crear un usuario",
+        problema: error || error.message
+    });
+  }
+};
+
+
+// -----------------------------------------------
+
+// Petición GET -> Mostrar todos los usuarios
+export const showUsers = async (req, res) => {
+  // manejo de errores -> atrapar lo que pueda salir mal
+  try {
+    // Encontrar TODOS los usuarios
+    let users = await userModel.find();
+    // validación si no se encuentran usuarios almacenados
+    if(users.length === 0){
+        return res.status(200).json({
+            mensaje: "No hay usuarios almacenados"
+        })
+    }
+
+    return res.status(200).json({
+        menasaje: "Se encontraron usuarios almacenados",
+        numeroUsuarios: users.length,
+        datos: users
+    })
+
+  } catch (error) {
+    return res.status(400).json({
+        mensaje: "Ocurrió un error al mostrar los usuarios",
+        problema: error || error.message
+    });
+  }
+};
---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,60 @@
+// 1. importar dependencias y modulos 
+import { json, request, response } from "express";
+import { verifyToken } from "../lib/jwt.js";
+
+// 2. Crearnos el MIDDLEWARE que nos permita usar la función para verificar el token
+// 2.1 Verigficar que existe un token generado 
+// 2.2 Verificar que el token sea permitido, que tenga la estructura alfa númerica 
+// 2.3 Validar el rol -> Verificar permisos
+
+function auth (requiredRol) { // esta es una funtion declarativa "ojo"
+        return async (request, response, next) => {
+            // Verificación 1: Existencia del token --------------
+            // Aqui estamos accediendo al token generando en caso si existiera 
+            let token = request.headers["authorization"];
+            console.log ( " token obtenido de la cabecera" + token);
+            if (!token){
+                return response.status(401).json({
+                    mensaje: "no se encontró token,  sorry! "
+                })
+                
+            }
+
+        // Verificación 2: Que el token sea permitido
+        // bearer eweeewewewewewewr4rregregtrg
+        // vamos a quitar el bearer para poder verificar bien el token 
+        token = token.split(" ")[1];
+        console.log("Token despues de separalo del bearer" + token);
+
+        // Manejo de errores
+        try {
+            const decoded = await verifyToken(token);
+            console.log("token decodificado" , decoded);
+            // Validación 3: Verificar rol 
+            // si se requiere arol de admin -> requiredRole === "admin"
+            // Pero si el usuario NO es admin 
+            // Mensaje de que no tiene permisos para esta petición
+            if(requiredRol === "admin" && !decoded.isAdmin ){
+                return response.status(401).json({
+                    mensaje: " acceso no permitido, sorry! no es administrador"
+                })
+
+            }
+        // es guardar la info decodificada en la petición
+        request. user = decoded;
+
+        }catch (error){
+            return response.status(400).json ({
+            mensaje: "fallo la autenticación del token",
+            problema: error.message || error
+            });  
+        }
+        // indica que debe continuar con el siguiente proceso
+        next();
+        }
+}
+
+
+// 3. Exportarlo -> asi exportarmos o solo le ponemos export a la function anterior 
+
+export default auth; 
