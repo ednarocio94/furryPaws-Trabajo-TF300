83bc2c613c50b7585c2e32e71a3d1ad94ca719eb
ednarocio94 <ednarociorodriguez94@gmail.com>
2024-11-14 20:07:34 -0500

    actualizando

--- Backend/src/models/admin.model.js
+++ Backend/src/models/admin.model.js
@@ -6,13 +6,13 @@ import mongoose from 'mongoose';
 
 // 2. plantilla de los datos se define como SCHEMA -> esquema de datos que vamos a solicitar para guardar en la base de datos
 
-const productSchema = new mongoose.Schema({
+const adminSchema = new mongoose.Schema({
     //este nombre lo eligen usted, son caracteristicas de la info que quiere guardar
     image:{type: String, required: true}, 
     fullName: {type: String, required:true},
     email: {type:String, required:true, unique:true},
     password: {type: String, required:true},
-    role:{type: String, default: 'admin 1'}
+    role:{type: String, default: 'admin'}
 });
 
 
@@ -20,4 +20,4 @@ const productSchema = new mongoose.Schema({
 // 3. Decirle a la base de datos que se cree una colección con el esquema anterior
 //el primer parámetro, es el nombre de la colección
 //el segundo parámetro, es la estructura de datos
-export const productModel = mongoose.model('product', productSchema);
+export const adminUserModel = mongoose.model('product', adminSchema);
--- Backend/src/models/mascotas.model.js
+++ Backend/src/models/mascotas.model.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 
 // 2. plantilla de los datos se define como SCHEMA -> esquema de datos que vamos a solicitar para guardar en la base de datos
 
-const productSchema = new mongoose.Schema({
+const mascotaSchema = new mongoose.Schema({
     //este nombre lo eligen usted, son caracteristicas de la info que quiere guardar
     image:{type: String, required: true}, 
     name: {type: String, required: true},
@@ -18,4 +18,4 @@ const productSchema = new mongoose.Schema({
 // 3. Decirle a la base de datos que se cree una colección con el esquema anterior
 //el primer parámetro, es el nombre de la colección
 //el segundo parámetro, es la estructura de datos
-export const productModel = mongoose.model('product', productSchema);
+export const mascotaModel = mongoose.model('product', mascotaSchema);
