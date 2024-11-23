b02b003d9a593b8b10e7d4842369dd389798a074
Jonathan200611613 <andressguilera2006@gmail.com>
2024-11-22 21:35:30 -0500

    subir cambios a git hub jonathan

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -4,9 +4,9 @@ import { createAdmin, showAdmin, deleteAdminById } from '../controllers/admin.co
 const routerAdmin = express.Router();
 
 // Rutas para la gestión de Admin por parte del Admin
-routerAdmin.post('/admin/crear', adminCreateUser);           // Crear un usuario
-routerAdmin.get('/admin', adminGetAllUsers);                 // Obtener todos los admin
-routerAdmin.delete('/admin/eliminar/:id', adminDeleteUser);  // Eliminar un usuario por ID
+routerAdmin.post('/admin/crear', createAdmin);           // Crear un usuario
+routerAdmin.get('/admin', showAdmin);                 // Obtener todos los admin
+routerAdmin.delete('/admin/eliminar/:id', deleteAdminById);  // Eliminar un usuario por ID
 
 
 export default routerAdmin;
