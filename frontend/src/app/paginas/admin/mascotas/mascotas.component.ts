932f00765ed93aea5ead65126b52e0c69eb9815d
Jonathan200611613 <andressguilera2006@gmail.com>
2024-11-29 22:35:05 -0500

    ya se importaron las paginas y se crearon las rutas

---
+++ frontend/src/app/paginas/admin/mascotas/mascotas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascotas',
+  imports: [],
+  templateUrl: './mascotas.component.html',
+  styleUrl: './mascotas.component.css'
+})
+export class MascotasComponent {
+
+}
---
+++ frontend/src/app/paginas/paws/paws.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paws',
+  imports: [],
+  templateUrl: './paws.component.html',
+  styleUrl: './paws.component.css'
+})
+export class PawsComponent {
+
+}
