f38cf4179f7add24a4d15c8debcec4abccaa4f6f
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-06 22:04:55 -0500

    nevbar y footer

--- frontend/src/app/paginas/paws/paws.component.ts
+++ frontend/src/app/paginas/paws/paws.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-paws',
-  imports: [],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './paws.component.html',
   styleUrl: './paws.component.css'
 })
