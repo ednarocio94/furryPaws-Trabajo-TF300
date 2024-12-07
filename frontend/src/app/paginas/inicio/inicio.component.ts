628b0046e4f77663825d91f629aa1cc504bb3743
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-06 20:43:25 -0500

    prueba error

--- frontend/src/app/paginas/inicio/inicio.component.ts
+++ frontend/src/app/paginas/inicio/inicio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../component/nav-bar/nav-bar.component';
+import { CarouselComponent } from "../../component/carousel/carousel.component";
+import { CardsComponent } from "../../component/cards/cards.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, CarouselComponent, CardsComponent, FooterComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
