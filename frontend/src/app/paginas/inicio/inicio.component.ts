e03c1744a3325ceb2d99cb5766058b34405b089f
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-09 21:35:18 -0500

    cambios en el navBar

--- frontend/src/app/paginas/inicio/inicio.component.ts
+++ frontend/src/app/paginas/inicio/inicio.component.ts
@@ -4,10 +4,11 @@ import { CarouselComponent } from "../../component/carousel/carousel.component";
 import { CardsComponent } from "../../component/cards/cards.component";
 import { FooterComponent } from "../../component/footer/footer.component";
 import { Cards2Component } from "../../component/cards2/cards2.component";
+import { Cards3Component } from "../../component/cards3/cards3.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavBarComponent, CarouselComponent, CardsComponent, FooterComponent, Cards2Component],
+  imports: [NavBarComponent, CarouselComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
