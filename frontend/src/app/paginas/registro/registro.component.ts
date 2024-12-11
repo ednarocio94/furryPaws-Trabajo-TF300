f4520ec3fb80e11b2e494bdcda1dac989bb722d2
Matias Schrader <matiasschrader2@gmail.com>
2024-12-10 21:46:22 -0500

    act formulario

--- frontend/src/app/paginas/registro/registro.component.ts
+++ frontend/src/app/paginas/registro/registro.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../component/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../component/footer/footer.component';
 
 @Component({
   selector: 'app-registro',
+  imports: [NavBarComponent,FooterComponent],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css'],
 })
