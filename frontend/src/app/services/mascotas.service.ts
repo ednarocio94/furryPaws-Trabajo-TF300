4a52738baa255f6f9a2ecdd3419ebec36781ade4
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-10 21:47:52 -0500

    backend del frontend

---
+++ frontend/src/app/services/mascotas.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Mascotas } from '../interfaces/mascotas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MascotasService {
+
+  // Inyección de dependencias
+  private _httpClient = inject(HttpClient);
+
+  // Ruta de conexión con el backend
+  private URL_MASCOTAS = 'http://localhost:3000/mascotas';
+
+  // Petición POST para crear una mascota
+  postMascota(mascota: Mascotas) {
+    return this._httpClient.post(this.URL_MASCOTAS + '/crear', mascota);
+  }
+
+  // Petición GET para obtener todas las mascotas
+  getMascotas() {
+    return this._httpClient.get(this.URL_MASCOTAS + '/obtener');
+  }
+
+  // Petición PUT para actualizar información de una mascota
+  putMascota(mascotaActualizada: Mascotas, id: string) {
+    return this._httpClient.put(this.URL_MASCOTAS + '/actualizar/' + id, mascotaActualizada);
+  }
+
+  // Petición DELETE para eliminar una mascota
+  deleteMascota(id: string) {
+    return this._httpClient.delete(this.URL_MASCOTAS + '/eliminar/' + id);
+  }
+}
