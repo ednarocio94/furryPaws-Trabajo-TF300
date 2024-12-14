cf08be3318061db7b221b9afe39755193e8debe4
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-13 22:52:14 -0500

    haciendo landing

--- frontend/src/app/paginas/inicio/inicio.component.ts
+++ frontend/src/app/paginas/inicio/inicio.component.ts
@@ -5,10 +5,11 @@ import { CardsComponent } from "../../component/cards/cards.component";
 import { FooterComponent } from "../../component/footer/footer.component";
 import { Cards2Component } from "../../component/cards2/cards2.component";
 import { Cards3Component } from "../../component/cards3/cards3.component";
+import { BannerComponent } from "../../component/banner/banner.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavBarComponent, CarouselComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component],
+  imports: [NavBarComponent, CarouselComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component, BannerComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
