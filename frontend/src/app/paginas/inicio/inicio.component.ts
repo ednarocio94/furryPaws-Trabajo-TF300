b9e108eb937f44bd93aeb580adc8bf1ec22fc1f1
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-17 17:51:27 -0500

    mirando un error

--- frontend/src/app/paginas/inicio/inicio.component.ts
+++ frontend/src/app/paginas/inicio/inicio.component.ts
@@ -6,10 +6,11 @@ import { Cards2Component } from "../../component/cards2/cards2.component";
 import { Cards3Component } from "../../component/cards3/cards3.component";
 import { BannerComponent } from "../../component/banner/banner.component";
 import { Card4Component } from "../../component/card4/card4.component";
+import { Card5Component } from "../../component/card5/card5.component";
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavBarComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component, BannerComponent, Card4Component],
+  imports: [NavBarComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component, BannerComponent, Card4Component, Card5Component],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
