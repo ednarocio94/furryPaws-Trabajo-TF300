76a33a7170895a6733cf1fe305475a300be36299
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-18 13:41:18 -0500

    corrigirndo carpetas

--- frontend/src/app/paginas/inicio/inicio.component.ts
+++ frontend/src/app/paginas/inicio/inicio.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
-import { CardsComponent } from "../../components/cards/cards.component";
+import { BannerComponent } from "../../components/banner/banner.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardsComponent } from "../../components/cards/cards.component";
 import { Cards2Component } from "../../components/cards2/cards2.component";
 import { Cards3Component } from "../../components/cards3/cards3.component";
-import { BannerComponent } from "../../components/banner/banner.component";
-import { Card4Component } from "../../component/card4/card4.component";
-import { Card5Component } from "../../component/card5/card5.component";
+import { Card4Component } from '../../components/card4/card4.component';
+import { Card5Component } from '../../components/card5/card5.component';
 
 @Component({
   selector: 'app-inicio',
-  imports: [NavBarComponent, CardsComponent, FooterComponent, Cards2Component, Cards3Component, BannerComponent, Card4Component, Card5Component],
+  imports: [NavBarComponent, BannerComponent, FooterComponent, CardsComponent, Cards2Component, Cards3Component, Card4Component, Card5Component],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
