b84a5820c4d34c81a3f8217c49bb6ad5eb26cf07
Jonathan200611613 <andressguilera2006@gmail.com>
2024-12-17 20:20:26 -0500

    carpetas login admin y registro admin

---
+++ frontend/src/app/paginas/registro-admin/registro-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-admin',
+  imports: [],
+  templateUrl: './registro-admin.component.html',
+  styleUrl: './registro-admin.component.css'
+})
+export class RegistroAdminComponent {
+
+}
