78c0319d2e4170174aaf3f8f87f63735c4687622
ednarocio94 <ednarociorodriguez94@gmail.com>
2024-12-19 19:16:29 -0500

    holi

--- frontend/src/app/components/dash-board/dash-board.component.ts
+++ frontend/src/app/components/dash-board/dash-board.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MascotasService } from '../../services/mascotas.service';
+import { Mascotas } from '../../interfaces/mascotas';
+import { ToastrService } from 'ngx-toastr';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dash-board',
-  imports: [],
+  imports: [ReactiveFormsModule, NgFor,NgIf],
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
 
 export class DashboardComponent {
-  products = [
-    {
-      id: 1,
-      name: 'Ejemplo Producto',
-      description: 'Esta es una descripción del producto.',
-      time: '30 minutos',
-      difficulty: 'Fácil',
-      price: 50,
-      image: 'https://via.placeholder.com/150'
-    }
-  ];
+  // products = [
+  //   {
+  //     id: 1,
+  //     name: 'Ejemplo Producto',
+  //     description: 'Esta es una descripción del producto.',
+  //     time: '30 minutos',
+  //     difficulty: 'Fácil',
+  //     price: 50,
+  //     image: 'https://via.placeholder.com/150'
+  //   }
+  // ];
+
+mascotasService = inject(MascotasService);
+toastrService = inject(ToastrService);
+
+allMacotas: Mascotas[] = [];
 
   addProduct() {
     console.log('Añadir producto');
